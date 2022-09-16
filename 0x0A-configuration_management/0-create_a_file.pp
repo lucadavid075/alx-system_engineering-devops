ae7b0760fe3d876d8213bbc25eba68d7be638673
lucadavid075 <daiveedlucas049@gmail.com>
2022-09-16 07:35:26 -0700

    [update] /tmp/school file created using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+$filepath = '/tmp/school'
+
+file { $filepath:
+  ensure  => file,
+  path    => $filepath,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
