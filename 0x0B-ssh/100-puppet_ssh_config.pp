acbe9f551c7663d7624102ba87685068c2837ffa
lucadavid075 <daiveedlucas049@gmail.com>
2022-09-18 21:17:48 -0700

    [another fix]

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # modify SSH config file
 file_line { 'password authentication':
   ensure => 'present',
-  path   => '~/.ssh/config',
+  path   => '~/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no'
 }
 file_line { 'Identity file':
   ensure  => 'present',
-  path    => '~/.ssh/config',
+  path    => '~/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school'
 }
