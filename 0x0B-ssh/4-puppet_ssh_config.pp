d399500395c39892d0c824a42c3ea3682c23b8ce
Luke David <daiveedlucas049@gmail.com>
2022-09-20 17:40:09 +0100

    another fix

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # modify SSH config file
-file { 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no'
+  line   => '^PasswordAuthentication no'
 }
-file { 'Declare identity file':
+file_line { 'Declare identity file':
   ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school'
+  line    => '^IdentityFile ~/.ssh/school'
 }
